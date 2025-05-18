4fe159fea9a626d29a6add0f2a38ace6a57f242e
Taha Baig <connectmtb@gmail.com>
2025-05-18 15:42:30 +0500

    config file changes updated

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
 export default nextConfig;
